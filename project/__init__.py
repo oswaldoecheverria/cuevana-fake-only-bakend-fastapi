93afed50ba3d4e5e0a86d43b107d351a17438ab0
oswaldo Echeverria <oswaldo.echeverria@outlook.com>
2023-04-02 00:16:38 -0500

    Modularizacion de rutas y detalle de rutas

--- project/__init__.py
+++ project/__init__.py
@@ -1,11 +1,17 @@
 # Llamamos a la clase FastApi
 from fastapi import FastAPI
+# Centralizamos las urls 
+from fastapi import APIRouter
 # Llamamos a la BD para su coneccion con la app 
 from .database import database as conection
 # Importamos los modelos de BD 
 from .database import User
 from .database import Movie
 from .database import UserReview
+# Importamos las instancias FASTApi modulos
+from .routers import user_router
+from .routers import review_router
+from .routers import movie_router
 
 
 # Creamos el servidor instanciando a la clase 
@@ -14,6 +20,17 @@ app = FastAPI(
     description = 'En esta web podras realizar reseñas a tus peliculas favoritas',
     version = '1'
 )
+
+# Centralizamos la urls 
+api_v1 = APIRouter(prefix='/api/v1')
+
+
+# Incluimos las rutas de los modulos junto al detalle de las url
+api_v1.app.include_router(user_router)
+api_v1.app.include_router(review_router)
+api_v1.app.include_router(movie_router)
+
+
 
 # Evento StartUp 
 @app.on_event('startup')
