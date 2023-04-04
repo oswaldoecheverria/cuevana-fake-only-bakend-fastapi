5cf5975f6fb5cfce37302b61221c692158efb115
oswaldo Echeverria <oswaldo.echeverria@outlook.com>
2023-04-04 17:54:04 -0500

    Endpoint reseñas, validacion llaves foraneas

--- project/routers/reviews.py
+++ project/routers/reviews.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter, HTTPException
 # Importamos los modelos de BD
 from ..database import UserReview
+from ..database import User
+from ..database import Movie
 # Importamos los schemas 
 from ..schemas import ReviewRequestModel
 from ..schemas import ReviewResponseModel
@@ -12,7 +14,15 @@ router = APIRouter(prefix='/reviews')
 
 @router.post('', response_model=ReviewResponseModel)
 async def create_review(user_review: ReviewRequestModel):
+
     
+    if User.select().where(User.id == user_review.user_id).first() is None:
+        raise HTTPException(status_code=404, detail='El usurio no se ha encontrado')
+
+    if Movie.select().where(Movie.id == user_review.movie_id).first() is None:
+        raise HTTPException(status_code=404, detail='Pelicula no encontrada')
+
+
     user_review = UserReview.create(
         # Las siguinetes variables son las creadas en el modelo 
         # de validacion ReviewRequestModel
