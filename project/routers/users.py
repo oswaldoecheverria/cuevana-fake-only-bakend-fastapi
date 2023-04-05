44dae682c280f954724f32a2ebcd95e77686b88e
oswaldo Echeverria <oswaldo.echeverria@outlook.com>
2023-04-04 22:51:21 -0500

    Endpoint login - Se puede logear y devuelve el usuario logeado

--- project/routers/users.py
+++ project/routers/users.py
@@ -5,6 +5,9 @@ from ..database import User
 # Llamamos a los modelos de validacion schemas 
 from ..schemas import UserRequestModel
 from ..schemas import UserResponseModel
+# Importamos librerias para login 
+from fastapi.security import HTTPBasicCredentials
+
 
 router = APIRouter(prefix='/users')
 
@@ -25,5 +28,27 @@ async def create_user(user: UserRequestModel):
     )
     
     return user
+
+
+
+
+# Login 
+@router.post('/login', response_model=UserResponseModel)
+async def login(credentials: HTTPBasicCredentials):
+
+    #Buscamos en la BD el primer registro que cumpla con la condicion 
+    user = User.select().where(User.username == credentials.username).first()
+
+    if user is None:
+        raise HTTPException(status_code=404, detail='Usuario no encontrado')
+    
+    #Comparamos las contrasenias
+    if user.password != User.create_password(credentials.password):
+        raise HTTPException(status_code=404, detail='Error de password')
+   
+
+    return user
+
+
 
 
